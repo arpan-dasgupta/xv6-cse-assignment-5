aaf71dbab91b8e2900bf3b5603a23b132f033b81
arpan-dasgupta <arpandasgupta0@gmail.com>
2019-11-02 22:26:58 +0530

    Waitx working

--- proc.h
+++ proc.h
@@ -1,13 +1,14 @@
 // Per-CPU state
+#define MLFQ
 struct cpu {
-  uchar apicid;               // Local APIC ID
-  struct context *scheduler;  // swtch() here to enter scheduler
-  struct taskstate ts;        // Used by x86 to find stack for interrupt
-  struct segdesc gdt[NSEGS];  // x86 global descriptor table
-  volatile uint started;      // Has the CPU started?
-  int ncli;                   // Depth of pushcli nesting.
-  int intena;                 // Were interrupts enabled before pushcli?
-  struct proc *proc;          // The process running on this cpu or null
+    uchar apicid;               // Local APIC ID
+    struct context *scheduler;  // swtch() here to enter scheduler
+    struct taskstate ts;        // Used by x86 to find stack for interrupt
+    struct segdesc gdt[NSEGS];  // x86 global descriptor table
+    volatile uint started;      // Has the CPU started?
+    int ncli;                   // Depth of pushcli nesting.
+    int intena;                 // Were interrupts enabled before pushcli?
+    struct proc *proc;          // The process running on this cpu or null
 };
 
 extern struct cpu cpus[NCPU];
@@ -25,44 +26,50 @@ extern int ncpu;
 // at the "Switch stacks" comment. Switch doesn't save eip explicitly,
 // but it is on the stack and allocproc() manipulates it.
 struct context {
-  uint edi;
-  uint esi;
-  uint ebx;
-  uint ebp;
-  uint eip;
+    uint edi;
+    uint esi;
+    uint ebx;
+    uint ebp;
+    uint eip;
 };
 
 enum procstate { UNUSED, EMBRYO, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
 // Per-process state
 struct proc {
-  uint sz;                     // Size of process memory (bytes)
-  pde_t *pgdir;                // Page table
-  char *kstack;                // Bottom of kernel stack for this process
-  enum procstate state;        // Process state
-  int pid;                     // Process ID
-  struct proc *parent;         // Parent process
-  struct trapframe *tf;        // Trap frame for current syscall
-  struct context *context;     // swtch() here to run process
-  void *chan;                  // If non-zero, sleeping on chan
-  int killed;                  // If non-zero, have been killed
-  struct file *ofile[NOFILE];  // Open files
-  struct inode *cwd;           // Current directory
-  char name[16];               // Process name (debugging)
-  // Change
-  int ctime;  // Creation time
-  int rtime;  // Running time
-  int etime;  // End time
+    uint sz;                     // Size of process memory (bytes)
+    pde_t *pgdir;                // Page table
+    char *kstack;                // Bottom of kernel stack for this process
+    enum procstate state;        // Process state
+    int pid;                     // Process ID
+    struct proc *parent;         // Parent process
+    struct trapframe *tf;        // Trap frame for current syscall
+    struct context *context;     // swtch() here to run process
+    void *chan;                  // If non-zero, sleeping on chan
+    int killed;                  // If non-zero, have been killed
+    struct file *ofile[NOFILE];  // Open files
+    struct inode *cwd;           // Current directory
+    char name[16];               // Process name (debugging)
+    // Change
+    int ctime;  // Creation time
+    int rtime;  // Running time
+    int etime;  // End time
 };
+
+void updateProc();
 
 // Process memory is laid out contiguously, low addresses first:
 //   text
 //   original data and bss
 //   fixed-size stack
 //   expandable heap
-
-// Change
-struct {
-  struct spinlock lock;
-  struct proc proc[NPROC];
-} ptable;
+#ifdef MLFQ
+struct proc_stat {
+    int pid;            // PID of each process
+    float runtime;      // Use suitable unit of time
+    int num_run;        // number of time the process is executed
+    int current_queue;  // current assigned queue
+    int ticks[5];  // number of ticks each process has received at each of the 5
+                   // priority queue
+};
+#endif
